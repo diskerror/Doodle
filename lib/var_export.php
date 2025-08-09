224b66b3c0b5e129cc987d429b79b8604a8916ab
Reid Woodbury Jr <reid@rawsound.com>
2025-08-09 14:24:04 -0700

    added wrapper to prevent it being defined twice

--- lib/var_export.php
+++ lib/var_export.php
@@ -5,53 +5,55 @@ namespace Library;
 /**
  * Behaves like var_export() but uses "[]" notation instead of "array()".
  *
- * @param $in
- * @param bool $return false
+ * @param        $in
+ * @param bool   $return false
  * @param string $indentStr '   ' (three spaces)
  * @return string
  */
-function var_export($in, bool $return = false, $indentStr = '   '): string
-{
-	static $indent = 0;
+if (!function_exists('Library\\var_export')) {
+    function var_export($in, bool $return = false, $indentStr = '   '): string
+    {
+        static $indent = 0;
 
-	switch (gettype($in)) {
-		case 'array':
-			$returnStr = "[\n";
-			$indent++;
-			foreach ($in as $k => $v) {
-				if (!is_resource($v)) {
-					if (!is_integer($k)) {
-						$k = "'$k'";
-					}
+        switch (gettype($in)) {
+            case 'array':
+                $returnStr = "[\n";
+                $indent++;
+                foreach ($in as $k => $v) {
+                    if (!is_resource($v)) {
+                        if (!is_integer($k)) {
+                            $k = "'$k'";
+                        }
 
-					$returnStr .= str_repeat($indentStr, $indent) . $k . ' => ' . var_export($v) . ",\n";
-				}
-			}
-			$returnStr .= str_repeat($indentStr, --$indent) . ']';
-			break;
+                        $returnStr .= str_repeat($indentStr, $indent) . $k . ' => ' . var_export($v) . ",\n";
+                    }
+                }
+                $returnStr .= str_repeat($indentStr, --$indent) . ']';
+                break;
 
-		case 'object':
-			$returnStr = str_repeat($indentStr, $indent++) . "{\n";
-			foreach ($in as $k => $v) {
-				if (!is_resource($v)) {
-					$returnStr .= str_repeat($indentStr, $indent) . "'$k' => " . var_export($v) . ",\n";
-				}
-			}
-			$returnStr .= str_repeat($indentStr, --$indent) . '}';
-			break;
+            case 'object':
+                $returnStr = str_repeat($indentStr, $indent++) . "{\n";
+                foreach ($in as $k => $v) {
+                    if (!is_resource($v)) {
+                        $returnStr .= str_repeat($indentStr, $indent) . "'$k' => " . var_export($v) . ",\n";
+                    }
+                }
+                $returnStr .= str_repeat($indentStr, --$indent) . '}';
+                break;
 
-		case 'resource':
-			$returnStr = '';
-			break;
+            case 'resource':
+                $returnStr = '';
+                break;
 
-		default:
-			$returnStr = \var_export($in, true);	//	Uses built-in function.
-	}
+            default:
+                $returnStr = \var_export($in, true);    //	Uses built-in function.
+        }
 
-	if ($return) {
-		return $returnStr;
-	}
+        if ($return) {
+            return $returnStr;
+        }
 
-	echo $returnStr, PHP_EOL;
-	return '';
-}
+        echo $returnStr, PHP_EOL;
+        return '';
+    }
+};
